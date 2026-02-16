87f8c98cd84c7ddd061c1e2df5715b4ba4215a04
Jerred Shepherd <github@sjer.red>
2026-02-15 17:20:53 -0800

    chore: moved to monorepo/packages/webring

--- plausible.js
+++
@@ -1,6 +0,0 @@
-// Add Plausible analytics script
-const script = document.createElement("script");
-script.defer = true;
-script.dataset.domain = "webring.sjer.red";
-script.src = "https://plausible.sjer.red/js/script.js";
-document.head.appendChild(script);
